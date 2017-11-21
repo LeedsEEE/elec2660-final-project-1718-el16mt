738a8fec430f3cf7c05781da093f4c2d18ebde99
Matthew Tuplin [el16mt] <el16mt@eentpc253.leeds.ac.uk>
2017-11-21 14:17:55 +0000

    Added Height, Weight, Age & Boot Sole Length sliders with labels displaying their values.

--- Skeasy/ViewController.h
+++ Skeasy/ViewController.h
@@ -9,6 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *heightLabel;
+@property (weak, nonatomic) IBOutlet UILabel *weightLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bslLabel;
+@property (weak, nonatomic) IBOutlet UILabel *skiLengthLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dinLabel;
+
+- (IBAction)heightSlider:(UISlider *)sender;
+- (IBAction)weightSlider:(UISlider *)sender;
+- (IBAction)ageSlider:(UISlider *)sender;
+- (IBAction)bslSlider:(UISlider *)sender;
+- (IBAction)calculateButton:(UIButton *)sender;
 
 
 @end
--- Skeasy/ViewController.m
+++ Skeasy/ViewController.m
@@ -26,4 +26,22 @@
 }
 
 
+- (IBAction)heightSlider:(UISlider *)sender {
+        self.heightLabel.text = [NSString stringWithFormat:@"%.2f m", sender.value];
+}
+
+- (IBAction)weightSlider:(UISlider *)sender {
+    self.weightLabel.text = [NSString stringWithFormat:@"%.0f kg", sender.value];
+}
+
+- (IBAction)ageSlider:(UISlider *)sender {
+    self.ageLabel.text = [NSString stringWithFormat:@"%.0f ", sender.value];
+}
+
+- (IBAction)bslSlider:(UISlider *)sender {
+    self.bslLabel.text = [NSString stringWithFormat:@"%.0f mm", sender.value];
+}
+
+- (IBAction)calculateButton:(UIButton *)sender {
+}
 @end

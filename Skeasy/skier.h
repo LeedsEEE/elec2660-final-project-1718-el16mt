5252c3c19b4fddd32fb6ecd78f4b24ef35fea8fb
Matthew Tuplin [el16mt] <el16mt@eentpc253.leeds.ac.uk>
2017-12-01 13:34:25 +0000

    Added logo to the storyboard. Added skill level picker with code to display beginner, intermediate and advanced.

---
+++ Skeasy/skier.h
@@ -0,0 +1,27 @@
+//
+//  skier.h
+//  Skeasy
+//
+//  Created by Matthew Tuplin [el16mt] on 01/12/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface skier : NSObject
+
+@property float height;
+@property float weight;
+@property float age;
+@property float bsl;
+
+@property int heightCategory;
+@property int weightCategory;
+@property int aveCategory;
+
+@property float skiLength;
+@property float DIN;
+
+
+
+@end
---
+++ Skeasy/skier.m
@@ -0,0 +1,13 @@
+//
+//  skier.m
+//  Skeasy
+//
+//  Created by Matthew Tuplin [el16mt] on 01/12/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "skier.h"
+
+@implementation skier
+
+@end
